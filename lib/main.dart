30ab3fe3eff101aa0a14422061e20f44dd73c488
rajpate1225 <rkumar79441@gmail.com>
2021-03-09 12:35:47 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -112,22 +112,3 @@ class _MyAppState extends State<MyApp> {
     );
   }
 }
-
-// class MyHomePage extends StatefulWidget {
-//   @override
-//   _MyHomePageState createState() => _MyHomePageState();
-// }
-
-// class _MyHomePageState extends State<MyHomePage> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('DeliMeals'),
-//       ),
-//       body: Center(
-//         child: Text('Navigation Time!'),
-//       ),
-//     );
-//   }
-// }
